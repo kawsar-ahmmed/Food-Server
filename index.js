28fac83a3d590ae80efa95951efeea62c69e1e2d
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-07 03:08:33 +0600

    post data

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = 3001;
 
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('nodemon worked auto restert')
-})
+    res.send('nodemon worked auto restart');
+});
+
 const users = [
-    { id: 1, name: "sabana", emial: "sabana@gmail.com", phone: "01299999922" },
-    { id: 2, name: "sabnooks", emial: "sabnooks@gmail.com", phone: "01299999922" },
-    { id: 3, name: "srabonti", emial: "srabonti@gmail.com", phone: "01299999922" },
-    { id: 4, name: "suchuna", emial: "suchuna@gmail.com", phone: "01299999922" },
-    { id: 5, name: "kobori", emial: "kobori@gmail.com", phone: "01299999922" },
-    { id: 6, name: "sabila", emial: "sabila@gmail.com", phone: "01299999922" },
-    { id: 7, name: "shakira", emial: "shakira@gmail.com", phone: "01299999922" },
-]
+    { id: 1, name: 'sabana', email: 'sabana@gmail.com', phone: '01299999922' },
+    { id: 2, name: 'sabnooks', email: 'sabnooks@gmail.com', phone: '01299999922' },
+    { id: 3, name: 'srabonti', email: 'srabonti@gmail.com', phone: '01299999922' },
+    { id: 4, name: 'suchuna', email: 'suchuna@gmail.com', phone: '01299999922' },
+    { id: 5, name: 'kobori', email: 'kobori@gmail.com', phone: '01299999922' },
+    { id: 6, name: 'sabila', email: 'sabila@gmail.com', phone: '01299999922' },
+    { id: 7, name: 'shakira', email: 'shakira@gmail.com', phone: '01299999922' },
+];
 
 app.get('/users', (req, res) => {
-    res.send(users)
+    res.send(users);
 });
 
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
-    const user = users[id];
-    res.send('User Finding')
-    console.log(user);
+    const user = users[id - 1];
+    res.send(user);
 });
+
+app.post('/users', (req, res) => {
+
+    console.log('request', req.body);
+    res.send('post method success');
+});
+
+app.get('/fruits', (req, res) => {
+    res.send(['mango', 'banana', 'oranges', 'apple']);
+});
+
 app.listen(port, () => {
-    console.log('Example app listening on port', port)
+    console.log('Example app listening on port', port);
 });
